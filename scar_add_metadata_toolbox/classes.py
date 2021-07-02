58a759507923c671f93d3be630137c0c0847f8f9
Felix Fennell <felnne@bas.ac.uk>
2021-07-02 14:54:08 +0100

    Fixing failed tests

--- scar_add_metadata_toolbox/classes.py
+++ scar_add_metadata_toolbox/classes.py
@@ -1853,7 +1853,7 @@ class Item:
             and bounding_box["south_latitude"] == -90
             and bounding_box["west_longitude"] == -180
         ):
-            bounding_polygon["features"][0]["crs"]["properties"]["name"] = "urn:ogc:def:crs:EPSG::3031"
+            bounding_polygon["crs"]["properties"]["name"] = "urn:ogc:def:crs:EPSG::3031"
             bounding_polygon["features"][0]["geometry"] = WellKnownExtents.ANTARCTICA.value
         elif (  # pragma: no cover (will be addressed in #116)
             bounding_box["east_longitude"] == 180
@@ -1861,7 +1861,7 @@ class Item:
             and bounding_box["south_latitude"] == -60
             and bounding_box["west_longitude"] == -180
         ):
-            bounding_polygon["features"][0]["crs"]["properties"]["name"] = "urn:ogc:def:crs:EPSG::3031"
+            bounding_polygon["crs"]["properties"]["name"] = "urn:ogc:def:crs:EPSG::3031"
             bounding_polygon["features"][0][
                 "geometry"
             ] = WellKnownExtents.SUB_ANTARCTICA.value  # pragma: no cover (will be addressed in #116)

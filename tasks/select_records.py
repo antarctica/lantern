0f0a3f6358d837681607a75695d458329007b835
Felix Fennell <felnne@bas.ac.uk>
2025-08-20 10:58:16 +0100

    Adding additional tasks for record loading
    Adds a task for selecting specific records from the cache for editing.
    
    Adds a task for bulk loading changed records from a directory (presumed to be a WMS pre-head type working copy of records repo) for making larger changes to records via other scripts etc.
    
    Existing load task renamed to import (which makes more sense).
    
    In time much of this will be refactored into a Repo class or similar to coordinate better.

---
+++ tasks/select_records.py
@@ -0,0 +1,111 @@
+import logging
+from pathlib import Path
+
+import inquirer
+
+from lantern.config import Config
+from lantern.log import init as init_logging
+from lantern.log import init_sentry
+from lantern.stores.base import Store
+from lantern.stores.gitlab import GitLabStore
+
+
+def _get_args() -> list[str]:
+    """Get user input for multiple file identifiers."""
+    file_identifiers = []
+
+    print("File identifiers can be URLs, file names, or bare identifiers.")
+    print("Examples:")
+    print("- https://example.com/items/123/index.html")
+    print("- https://example.com/items/123/")
+    print("- https://example.com/items/123")
+    print("- 123.json")
+    print("- 123")
+
+    while True:
+        answers = inquirer.prompt(
+            [
+                inquirer.Text("record", message="File Identifier"),
+                inquirer.Confirm("continue", message="Add another?", default=False),
+            ]
+        )
+        file_identifiers.append(answers["record"])
+        if not answers["continue"]:
+            break
+    return file_identifiers
+
+
+def _process_selections(file_identifiers: list[str]) -> None:
+    """
+    Process file identifiers into bare identifiers.
+
+    Handles common references such as URLs or file names for convenience.
+    """
+    for i, fid in enumerate(file_identifiers):
+        if "https://" in fid:
+            # for 'https://example.com/items/123' or 'https://example.com/items/123/' or 'https://example.com/items/123/index.html' as '123'
+            file_identifiers[i] = fid.replace("index.html", "").rstrip("/").split("/")[-1]
+        if ".json" in fid:
+            # for '123.json' use '123'
+            file_identifiers[i] = fid.replace(".json", "")
+
+
+def _confirm_selection(file_identifiers: list[str]) -> bool:
+    """Confirm the selected file identifiers with the user."""
+    print("Selected records:")
+    for file_identifier in file_identifiers:
+        print(f"- {file_identifier}")
+
+    answers = inquirer.prompt(
+        [
+            inquirer.Confirm("confirm", message="Confirm selection?", default=True),
+        ]
+    )
+    return answers["confirm"]
+
+
+def _dump_records(logger: logging.Logger, file_identifiers: list[str], store: Store, output_path: Path) -> None:
+    """Dump selected records from the store to a path."""
+    output_path.mkdir(parents=True, exist_ok=True)
+    for file_identifier in file_identifiers:
+        record_path = output_path / f"{file_identifier}.json"
+        record = store.get(file_identifier)
+        logger.debug(f"Writing {record_path.resolve()}")
+        with record_path.open(mode="w") as f:
+            f.write(record.dumps_json())
+
+
+def main() -> None:
+    """Entrypoint."""
+    init_logging()
+    init_sentry()
+    logger = logging.getLogger("app")
+    logger.info("Initialising")
+
+    config = Config()
+    store = GitLabStore(
+        logger=logger,
+        endpoint=config.STORE_GITLAB_ENDPOINT,
+        access_token=config.STORE_GITLAB_TOKEN,
+        project_id=config.STORE_GITLAB_PROJECT_ID,
+        cache_path=config.STORE_GITLAB_CACHE_PATH,
+    )
+
+    file_identifiers = _get_args()
+    _process_selections(file_identifiers)
+    if not _confirm_selection(file_identifiers):
+        logger.info("Selection rejected by user.")
+        return
+
+    import_path = Path("./import")
+    logger.info("Loading records from Store")
+    store.populate()
+
+    logger.info(f"Dumping {len(file_identifiers)} selected records from Store")
+    _dump_records(logger, file_identifiers, store, import_path)
+
+    logger.info(f"{len(file_identifiers)} records in {import_path.resolve()} for editing.")
+
+
+if __name__ == "__main__":
+    main()

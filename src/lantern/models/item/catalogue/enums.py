49fbd957600c7917c5a4ab6449dd7f82f6073e26
Felix Fennell <felnne@bas.ac.uk>
2025-10-05 11:48:26 +0100

    Tidying up

--- src/lantern/models/item/catalogue/enums.py
+++ src/lantern/models/item/catalogue/enums.py
@@ -17,19 +17,19 @@ class DistributionType(Enum):
     ARCGIS_OGC_FEATURE_LAYER = "OGC API Features (ArcGIS)"
     ARCGIS_RASTER_TILE_LAYER = "ArcGIS Raster Tile Layer"
     ARCGIS_VECTOR_TILE_LAYER = "ArcGIS Vector Tile Layer"
+    CSV = "CSV"
+    FPL = "FPL"
     GEOJSON = "GeoJSON"
     GEOPACKAGE = "GeoPackage"
     GEOPACKAGE_ZIP = "GeoPackage (Zipped)"
+    GPX = "GPX"
     JPEG = "JPEG"
+    MAPBOX_VECTOR_TILE = "MapBox Vector Tiles (MBTile)"
     PDF = "PDF"
     PDF_GEO = "PDF (Georeferenced)"
     PNG = "PNG"
     SHAPEFILE_ZIP = "Shapefile (Zipped)"
     X_PAPER_MAP = "Flat or folded paper map"
-    CSV = "CSV"
-    FPL = "FPL"
-    GPX = "GPX"
-    MAP_BOX_VECTOR_TILE = "MapBox Vector Tiles (MBTile)"
 
 
 class Licence(Enum):

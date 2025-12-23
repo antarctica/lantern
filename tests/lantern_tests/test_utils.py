61258cc41f10c1d2830501183f00651c61d0c1a0
Felix Fennell <felnne@bas.ac.uk>
2025-12-23 23:21:11 +0000

    Coverage test

--- tests/lantern_tests/test_utils.py
+++ tests/lantern_tests/test_utils.py
@@ -4,7 +4,7 @@ import pytest
 
 from lantern.config import Config
 from lantern.stores.gitlab import GitLabStore
-from lantern.utils import init_gitlab_store
+from lantern.utils import init_gitlab_store, init_s3_client
 
 
 @pytest.mark.cov()
@@ -19,3 +19,13 @@ class TestUtils:
         """
         store = init_gitlab_store(logger=fx_logger, config=fx_config)
         assert isinstance(store, GitLabStore)
+
+    @pytest.mark.cov()
+    def test_s3_client(self, fx_logger: logging.Logger, fx_config: Config):
+        """
+        Can init S3 client.
+
+        Initially a coverage only check.
+        """
+        client = init_s3_client(config=fx_config)
+        assert client is not None

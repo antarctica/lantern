6dbf102ceb79d8183712115365733a3c3e7b87e0
Felix Fennell <felnne@bas.ac.uk>
2026-01-15 13:49:41 +0000

    Refactoring dev task

--- tasks/cache_init.py
+++ tasks/cache_init.py
@@ -1,17 +1,25 @@
+import logging
+
 from tasks._record_utils import confirm_source, init
 
+from lantern.stores.gitlab_cache import GitLabCachedStore
 
-# noinspection PyProtectedMember
-def main() -> None:
-    """Entrypoint."""
-    logger, _config, store, _s3, _keys = init()
 
+def init_cache(logger: logging.Logger, store: GitLabCachedStore) -> None:
+    """Initialise cache in store."""
     confirm_source(logger=logger, store=store, action="Selecting records from")
     store.purge()
     store._cache._ensure_exists()
     if not store._cache.exists:
         msg = "Could not initialise cache."
         raise RuntimeError(msg) from None
+
+
+# noinspection PyProtectedMember
+def main() -> None:
+    """Entrypoint."""
+    logger, _config, store, _s3 = init()
+    init_cache(logger=logger, store=store)
 
 
 if __name__ == "__main__":
